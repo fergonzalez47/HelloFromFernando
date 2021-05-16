99c434d78be8baa49b9cebc1c010fdea3ad139dd
HECTOR RODRIGUEZ <hector_531@hotmail.com>
2021-05-16 16:00:38 -0400

    Hector

--- src/Hello.java
+++ src/Hello.java
@@ -2,6 +2,6 @@ public class Hello {
 
      public static void main(String[] args) {
          System.out.println("Hello ! My name is Fernando");
-
+         System.out.println("Hello Fernando, my name is Hector.");
     }
 }
